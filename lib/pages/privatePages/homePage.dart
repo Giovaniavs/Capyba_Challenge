18b57f3882777d19b0e330033c85ceb448ca4488
Giovaniavs <gavs2@cin.ufpe.br>
2021-05-02 15:42:00 -0300

    Feature(homePage): created a base of homePage view and created a drawer with buttons

--- lib/pages/privatePages/homePage.dart
+++ lib/pages/privatePages/homePage.dart
@@ -11,28 +11,87 @@ class _HomePageState extends State<HomePage> {
   String email = '';
   String password = '';
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openDrawer() {
+    _scaffoldKey.currentState.openDrawer();
+  }
+
+  void _closeDrawer() {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ButtonComponent(
-                    width: 90,
-                    title: 'Entrar',
-                    backgroundColor: Colors.greenAccent.shade700,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            )),
+      appBar: AppBar(
+        title: Text(
+          'Home Page',
+          style: TextStyle(color: Colors.white),
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
       ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 30.0,
+            left: 8.0,
+            right: 8.0,
+            bottom: 8.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Column(
+                  children: [
+                    ButtonComponent(
+                      backgroundColor: Colors.greenAccent.shade700,
+                      fontSize: 20,
+                      height: 50,
+                      onPressed: _closeDrawer,
+                      width: double.infinity,
+                      title: 'Meu Perfil',
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ButtonComponent(
+                      backgroundColor: Colors.blueAccent.shade400,
+                      fontSize: 20,
+                      height: 50,
+                      onPressed: _closeDrawer,
+                      width: double.infinity,
+                      title: 'Validar Email',
+                    ),
+                  ],
+                ),
+              ),
+              ButtonComponent(
+                backgroundColor: Colors.red.shade400,
+                fontSize: 20,
+                height: 50,
+                onPressed: _closeDrawer,
+                width: double.infinity,
+                title: 'Sair',
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.greenAccent.shade700,
+        onPressed: () {},
+        tooltip: 'Increment Counter',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
