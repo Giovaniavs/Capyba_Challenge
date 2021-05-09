fe21df2c08dcc5b40868b27545475ad286a132a2
Giovaniavs <gavs2@cin.ufpe.br>
2021-05-09 19:55:05 -0300

    Hotfix(provider): remove the user provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:capyba_challenge/services/authProvider.dart';
-import 'package:capyba_challenge/services/authServices.dart';
-
 import 'package:capyba_challenge/customWidgets/customSplashScreen.dart';
 
 import 'package:capyba_challenge/pages/publicPages/successRegisterPage.dart';
@@ -20,27 +17,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return UserProvider(
-      auth: AuthService(),
-      child: MaterialApp(
-        title: 'Capyba Challenge',
-        theme: ThemeData(
-          fontFamily: 'Roboto',
-          primaryColor: Colors.greenAccent.shade700,
-        ),
-        home: CustomSplashScreen(
-          navigateTo: LandingPage(),
-        ),
-        routes: {
-          '/landingPage': (context) => LandingPage(),
-          '/register': (context) => RegisterPage(),
-          '/successRegister': (context) => SuccessRegisterPage(),
-          '/login': (context) => LoginPage(),
-          '/home': (context) => HomePage(),
-          '/restrictedPage': (context) => RestrictedPage(),
-          '/myProfile': (context) => MyProfilePage(),
-        },
+    return MaterialApp(
+      title: 'Capyba Challenge',
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        primaryColor: Colors.greenAccent.shade700,
       ),
+      home: CustomSplashScreen(
+        navigateTo: LandingPage(),
+      ),
+      routes: {
+        '/landingPage': (context) => LandingPage(),
+        '/register': (context) => RegisterPage(),
+        '/successRegister': (context) => SuccessRegisterPage(),
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/restrictedPage': (context) => RestrictedPage(),
+        '/myProfile': (context) => MyProfilePage(),
+      },
     );
   }
 }
